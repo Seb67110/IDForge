a43c6ada792cb39b7b748bd5af44d8661960eeb3
Sébastien SCHNEPP <seb.schnepp@outlook.com>
2025-03-22 18:21:38 +0100

    Added the first replication module code

---
+++ replication/Events/OnAccountCreate.ps1
@@ -0,0 +1,93 @@
+﻿Param([String]$POSTVariables,[Int]$DBUserID)
+
+# Répertoire local du script
+$path = "$([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition))"
+
+# Converting the post variables sent as a string format to a hashtable
+$POSTVariables_Hashtable = $POSTVariables.Split(";") | ConvertFrom-StringData
+
+
+#$test = $POSTVariables_Hashtable.GetEnumerator().first_name
+#$test = [System.Text.Encoding]::BigEndianUnicode.GetString([System.Text.Encoding]::UTF8.GetBytes($POSTVariables))
+# [System.Text.Encoding]::UTF8.GetBytes($test)
+
+# Gets the scripts in the subpath
+$subscripts = Get-ChildItem -Path "$path\OnAccountCreate\*" -File -Include *.ps1
+
+# Database parameters
+$serverName = "sql_prd.sch.corp"
+$databaseName = "mydb"
+$username = "sims_ps"
+$password = "Admin4ever" 
+
+#$connectionString = "server=$serverName;user id=$username;password=$password;database=$databaseName;pooling=false"
+$connectionString = "Server=$serverName;Database=$databaseName;User Id=$username;Password=$password;"
+$connectionStringNamedPipes = "Server=np:\\$serverName\pipe\sql\query;Database=$databaseName;User Id=$username;Password=$password;"
+
+# Connecting to the SQL server
+[void][Reflection.Assembly]::LoadWithPartialName('MySQL.Data')
+$connection = New-Object MySql.Data.MySqlClient.MySqlConnection
+$connection.ConnectionString = $connectionString
+$connection.Open()
+
+# Looping through subscripts (excludes when filename begins by _)
+foreach ($item in $subscripts | ? {$_.Name -notlike "_*"})
+{
+
+    # Executing the subscript
+    $output = & $item.FullName $POSTVariables_Hashtable
+
+    # If the subscript has not been skipped by its conditions
+    if ($output -ne $null)
+    {
+     
+        # If there is an error that has been caught, logs it   
+        If ($output.ErrorNumber -ne 0)
+        {
+
+            # Creating the CSV line for the error log
+            $csv = New-Object PSObject -Property @{
+
+                Timestamp     = Get-Date
+                ErrorNumber   = $output.ErrorNumber
+                ErrorMessage  = $output.ErrorMessage
+                POSTVariables = $POSTVariables
+                DBUserID      = $DBUserID
+            
+            }
+
+            # Ecriture de la ligne dans le fichier
+            Export-CSV -Path "$path\OnAccountCreate_ErrorLog.csv" -Delimiter "#" -InputObject $csv -Append -Encoding UTF8 -NoTypeInformation
+        
+        } Else { # External account created with success. Primary information has to be saved into DB
+
+        #write-host "OK" $item.FullName
+                    
+            try {
+
+                # SQL command
+                $query = "CALL add_externalaccount($DBUserID,'$($output.AccountName)','$($output.PrimaryName)','$($output.Primary)');"
+    
+                # Creating command object
+                $command = $connection.CreateCommand()
+                $command.CommandText = $query
+
+                # Actual execution
+                $result = $command.ExecuteReader()  
+            
+            } catch { <# Implement error handling #> }                     
+            
+        }
+
+    }
+
+}
+
+# Closing SQL connection
+$connection.Close()
+
+
+
+
+# Should return an error code or success message
+#Return "PowerShell return : $($POSTVariables)"
---
+++ replication/Events/OnAccountCreate/Service Now creation.ps1
@@ -0,0 +1,110 @@
+﻿Param([Object]$POSTVariables)
+
+If (
+
+# USER CONDITIONS *****************************************
+# Use $POSTVariables to do some conditions (if Service Now
+# checkbox is checked, the implemented code will be 
+# executed. Else, the script will return $null
+
+    $true -eq $true # Dummy condition (always true)
+    #$true -eq $false # Dummy condition (always false)
+    #$POSTVariables.extaccount_servicenow -eq "true"
+    #$POSTVariables.something -eq "Condition"
+
+
+
+# END USER CONDITIONS *************************************
+
+) {} else {return $null}
+
+
+# USER IMPLEMENTATION *************************************
+# Implement here the creation of the external account
+
+    # Set your own constants in the following variables
+    $accountName     = "Service Now"   # This is the display name of the service associated to this script (Ex. Service Now, Exchange or AzureAD)
+    $primaryName     = "Sys_ID"      # The display name of the primary key (SysID for Service Now, GUID for Exchange, ObjectID for AzureAD, ...)
+
+    # Set your own variables in the return values 
+    # (if error, will be logged, if success, the primary keys
+    # for the external system will be commited in the DB
+        
+        #$primary         = "1234"  # The actual primary key to the account on the external system
+        $errorNumber     = 0        # If something goes wrong, this is different than 0 (use your own numbers, will be logged)
+        $errorMessage    = ""       # A display message that will be added to the log in case ErrorNumber is different than 0 (ignored if 0)
+
+
+    # Web service URL
+    $url = "https://dev196397.service-now.com/create_user.do?SOAP"
+    
+    # Credentials for the web-service connection
+    $username = "soap.guest"
+    $password = "Dev4ever" | ConvertTo-SecureString -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential($username, $password)
+
+    # Create a hash table with request parameters
+    $params = @{
+        Uri      = "https://dev196397.service-now.com/create_user.do?SOAP"
+        Method   = "POST"
+        Credential = $credential  
+        ContentType = "text/xml"
+        #Verbose    = $true
+    }
+
+    $body = "
+    <soapenv:Envelope xmlns:soapenv=`"http://schemas.xmlsoap.org/soap/envelope/`">
+        <soapenv:Header/>
+        <soapenv:Body>
+            <execute>
+                <first_name>$($POSTVariables.first_name)</first_name>
+                <last_name>$($POSTVariables.last_name)</last_name>
+                <title>$($POSTVariables.job_title)</title>
+                <mail>$($POSTVariables.mail)</mail>
+            </execute>
+        </soapenv:Body>
+    </soapenv:Envelope>"
+
+    Try {
+
+        # Using SSL1.2
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+
+        # Make the HTTP request
+        $response = Invoke-RestMethod @params -Body $body
+
+        # Once the Service Now account is created, returns the sys_id
+        $primary = $response.Envelope.Body.executeResponse.sys_id
+
+    } Catch {
+
+        # Error Number is set to 1
+        $errorNumber = 1
+
+        # Creates the error message for the log
+        [String]$errorMessage = $_.Exception
+    
+        # If available, adds the detailed error message from the response
+        if ($_.Exception.Response -ne $null) {
+        
+            $errorResponseStream = $_.Exception.Response.GetResponseStream()
+            $errorResponse = (New-Object IO.StreamReader($errorResponseStream)).ReadToEnd()
+        
+            $errorMessage = "$errorMessage `nDetailed error response: $errorResponse"
+    
+        }
+
+    }
+
+
+# END USER IMPLEMENTATION *********************************
+
+Return @{
+
+    AccountName     = $accountName
+    PrimaryName     = $primaryName
+    Primary         = $primary
+    ErrorNumber     = $errorNumber
+    ErrorMessage    = $errorMessage
+
+}
